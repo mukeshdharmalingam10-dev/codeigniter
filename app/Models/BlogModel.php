0624e08cf3e2fdf52c9e0ccf29fa68b6cb271c77
Mukesh N <mukeshdharmalingam10@gmail.com>
2026-02-10 15:28:57 +0530

    done

---
+++ app/Models/BlogModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BlogModel extends Model
+{
+    protected $table      = 'blogs';       // database table name
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = [
+        'title',
+        'content',
+        'created_at'
+    ];
+}
