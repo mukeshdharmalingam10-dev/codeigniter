a83d1af30bbc604e200ab9cdf9373bb7e9320f3c
Mukesh N <mukeshdharmalingam10@gmail.com>
2026-02-12 19:02:10 +0530

    push

--- app/Views/blog.php
+++ app/Views/blog.php
@@ -1,89 +1,162 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-      <title>Ci Blog</title>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ci Blog</title>
 
+    <!-- Import Google Icon Font -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
-
-    <body>
-<div class="container">
-    <div class="row">
-    <div class="col s12 m12">
-      <div class="card-panel">
-          <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">Blog<a class="right waves-effect waves-light btn-large modal-trigger" href="#modal1">Create Blog</a></span>
-           <div id="modal1" class="modal">
-    <div class="modal-content">
-      <div class="row">
-    <div class="input-field col s12">
-      <input value="Alvin" id="first_name2" type="text" class="validate">
-      <label class="active" for="first_name2">First Name</label>
-    </div>
-  </div>
-        </div>
+    <!-- Import Materialize CSS -->
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+    >
+</head>
+<body>
+    <div class="container section">
         <div class="row">
-          <div class="input-field col s12">
-            <textarea id="textarea2" class="materialize-textarea" data-length="120"></textarea>
-            <label for="textarea2">Textarea</label>
-          </div>
-        </div>
-      </form>
-    </div>
-          
-    </div>
-    <div class="modal-footer">
-      <a href="#!" class="modal-close waves-effect waves-green btn">create post</a>
-    </div>
-  </div>
-          <table>
-        <thead>
-          <tr>
-              <th>Name</th>
-              <th>Item Name</th>
-              <th>Item Price</th>
-          </tr>
-        </thead>
+            <div class="col s12">
+                <div class="card-panel">
+                    <div class="card-content">
+                        <span class="card-title grey-text text-darken-4">
+                            Blog
+                            <a class="right waves-effect waves-light btn modal-trigger" href="#createModal">
+                                Create Blog
+                            </a>
+                        </span>
 
-        <tbody>
-          <tr>
-            <td>Alvin</td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-            <td><button class="btn waves-effect waves-light">Edit</button></td>
-            <td><button class="btn red waves-effect waves-light">Delete</button></td>
-          </tr>
-          <tr>
-            <td>Alan</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-            <td><button class="btn waves-effect waves-light">Edit</button></td>
-            <td><button class="btn red waves-effect waves-light">Delete</button></td>
-          </tr>
-          <tr>
-            <td>Jonathan</td>
-            <td>Lollipop</td>
-            <td>$7.00</td>
-          </tr>
-        </tbody>
-      </table>
+                        <table class="striped responsive-table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Item Name</th>
+                                    <th>Item Price</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Alvin</td>
+                                    <td>Eclair</td>
+                                    <td>$0.87</td>
+                                    <td>
+                                        <button data-target="editModal1" class="btn waves-effect waves-light modal-trigger">Edit</button>
+                                        <button class="btn red waves-effect waves-light">Delete</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Alan</td>
+                                    <td>Jellybean</td>
+                                    <td>$3.76</td>
+                                    <td>
+                                        <button data-target="editModal2" class="btn waves-effect waves-light modal-trigger">Edit</button>
+                                        <button class="btn red waves-effect waves-light">Delete</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Jonathan</td>
+                                    <td>Lollipop</td>
+                                    <td>$7.00</td>
+                                    <td>
+                                        <button class="btn disabled">Edit</button>
+                                        <button class="btn red disabled">Delete</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-      </div>
+
+    <!-- Create Modal -->
+    <div id="createModal" class="modal">
+        <div class="modal-content">
+            <h4>Create New Blog Post</h4>
+            <form>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="create_name" type="text" class="validate">
+                        <label for="create_name">Name</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <textarea id="create_content" class="materialize-textarea" data-length="120"></textarea>
+                        <label for="create_content">Content</label>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn">Create Post</a>
+        </div>
     </div>
-  </div>
-   </div>
-      <!--JavaScript at end of body for optimized loading-->
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<script>
-  $(document).ready(function(){
-    $('.modal').modal();
-  }); </script>
-    </body>
-  </html>
+
+    <!-- Edit Modal 1 -->
+    <div id="editModal1" class="modal">
+        <div class="modal-content">
+            <h4>Edit Post</h4>
+            <form>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input value="Alvin" id="edit_name_1" type="text" class="validate" readonly>
+                        <label class="active" for="edit_name_1">Name</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <textarea id="edit_content_1" class="materialize-textarea" readonly>Eclair</textarea>
+                        <label class="active" for="edit_content_1">Content</label>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn">Update Post</a>
+        </div>
+    </div>
+
+    <!-- Edit Modal 2 -->
+    <div id="editModal2" class="modal">
+        <div class="modal-content">
+            <h4>Edit Post</h4>
+            <form>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input value="Alan" id="edit_name_2" type="text" class="validate" readonly>
+                        <label class="active" for="edit_name_2">Name</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <textarea id="edit_content_2" class="materialize-textarea" readonly>Jellybean</textarea>
+                        <label class="active" for="edit_content_2">Content</label>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn">Update Post</a>
+        </div>
+    </div>
+
+    <!-- JavaScript at end of body for optimized loading -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const modals = document.querySelectorAll('.modal');
+            M.Modal.init(modals);
+        });
+    </script>
+</body>
+</html>
